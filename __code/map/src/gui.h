add8818992918ed5c14db7da9a0dfd0818b5e112
TheGamerCant <123405360+TheGamerCant@users.noreply.github.com>
2024-11-07 14:13:10 +0000

    crap

--- __code/map/src/gui.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "raylib.h"
-
-namespace GUI {
-	bool inBounds(Vector2 mousePos, const int screenWidth, const int screenHeight, const int toolbarWidth) {
-		return mousePos.x < (screenWidth - toolbarWidth);
-	}
-}
---
+++ common/defines/MOD_defines.lua
@@ -0,0 +1,3 @@
+
+NDefines.NGame.START_DATE = "1980.1.1.1"		--base is 1936.1.1.12
+NDefines.NGame.END_DATE = "2001.9.11.10"		--base is 1949.1.1.1
