b89fb82e4b6597fc5e40d40208b6b6f235c75438
TheGamerCant <123405360+TheGamerCant@users.noreply.github.com>
2025-04-07 12:14:49 +0100

    More map stuff

--- __code/random rgb values/rgb_generator.py
+++ __code/random rgb values/rgb_generator.py
@@ -1,4 +1,4 @@
 import random
 
-for i in range(25):
+for i in range(30):
     print(str(random.randrange(0, 256)) + ";" + str(random.randrange(0, 256)) + ";" + str(random.randrange(0, 256)))		#256 non inclusive
