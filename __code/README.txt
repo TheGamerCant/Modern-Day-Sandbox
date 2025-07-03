62446dd1b6780799060285abbdf9a9fceb812413
TheGamerCant <123405360+TheGamerCant@users.noreply.github.com>
2025-07-03 11:26:54 +0100

    Test 1

---
+++ __code/README.txt
@@ -0,0 +1,11 @@
+___animated_images_legacy:
+	- Old code for animated Images in C++, obsolete
+
+animated_images:
+	- Code in C++ and Python for animated images
+	- Put .png images in the "in" folder and run "GUI.py"
+	- Outputs .png, .gui and .gfx files in the out folder
+	
+building_and_vp_fixer:
+	- Code in C++ to make sure buildings and VPs are in the right place
+	- Put buildings and units that you want to be consistant in the buildings and unitstacks files, then run "mainCode.exe"
--- __code/building_and_vp_fixer/mainCode.cpp
+++ __code/building_and_vp_fixer/mainCode.cpp
@@ -15,11 +15,11 @@ struct mapTypeStruct {
 };
 
 void replaceEntriesFunction(const std::filesystem::path& modDirectory, const std::string& fileName){
-	std::filesystem::path baseFilePath = fileName + ".txt";
-	std::filesystem::path modFilePath = modDirectory / "map" / fileName + ".txt";
-	std::filesystem::path tempFilePath = modDirectory / "map" / fileName + ".tmp";
+	std::filesystem::path baseFilePath = (fileName + ".txt");
+	std::filesystem::path modFilePath = modDirectory / "map" / (fileName + ".txt");
+	std::filesystem::path tempFilePath = modDirectory / "map" / (fileName + ".tmp");
 	
-	std::ifstream mapTypesToReplaceFile(fileName);
+	std::ifstream mapTypesToReplaceFile(baseFilePath);
     if (!mapTypesToReplaceFile) {
         std::cerr << baseFilePath << " does not exist.\n";
         return;
@@ -49,8 +49,8 @@ void replaceEntriesFunction(const std::filesystem::path& modDirectory, const std
 	//Now replace the entries in the mod buildings.txt
 	
 	//Start by making sure the file exists
-	std::ifstream buildingsToRepalceFile(modFilePath);
-	if (!buildingsToRepalceFile) {
+	std::ifstream inputFile(modFilePath);
+	if (!inputFile) {
 		std::cerr << modFilePath << " does not exist.\n";
 		return;
 	}
@@ -58,24 +58,37 @@ void replaceEntriesFunction(const std::filesystem::path& modDirectory, const std
 	
 	//Make a map for quicker look up
 	std::unordered_map<std::string, std::string> replacementMap;
-    for (const auto& entry : replacements) {
+    for (const auto& entry : mapTypesToReplaceVector) {
         replacementMap[entry.id] = entry.data;
     }
-	
-	int index = 0;
-	while (std::getline(mapTypesToReplaceFile, line)) {
+
+	while (std::getline(inputFile, line)) {
         size_t first_sep = line.find(';');
-        if (first_sep == std::string::npos) continue;
+        if (first_sep == std::string::npos) {
+            outputFile << line << '\n';
+            continue;
+        }
 
         size_t second_sep = line.find(';', first_sep + 1);
-        if (second_sep == std::string::npos) continue;
+        if (second_sep == std::string::npos) {
+            outputFile << line << '\n';
+            continue;
+        }
 
-        std::string part1 = line.substr(0, second_sep);
-		
-		++index;
+        std::string key = line.substr(0, second_sep);
+        auto it = replacementMap.find(key);
+
+        if (it != replacementMap.end()) {
+            outputFile << key << ";" << it->second << '\n';
+        } else {
+            outputFile << line << '\n';
+        }
     }
 	
-	std::filesystem::rename(tempFilePath, modDirectory);
+	inputFile.close();
+	outputFile.close();
+	
+	std::filesystem::rename(tempFilePath, modFilePath);
 }
 
 int main() {
@@ -84,9 +97,9 @@ int main() {
 	modDirectory = modDirectory.parent_path();
 	
 	std::thread t1(replaceEntriesFunction, modDirectory, "buildings");
-//	std::thread t2(stdSpriteFunction, modDirectory, "gfx\\interface\\decisions", "interface\\MOD_decisions.gfx");
+	std::thread t2(replaceEntriesFunction, modDirectory, "unitstacks");
 	
 	t1.join();
-//	t2.join();
+	t2.join();
 	return 0;
 }
