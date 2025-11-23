8b1e4905a1eb02cba7b96388590e2ac4a6e52e23
TheGamerCant <123405360+TheGamerCant@users.noreply.github.com>
2025-11-23 19:36:12 +0000

    Libya done

--- common/defines/01_TDA.lua
+++ common/defines/01_TDA.lua
@@ -2,7 +2,7 @@
 NDefines.NGame.START_DATE = "2010.1.1.1"		--base is 1936.1.1.12
 NDefines.NGame.END_DATE = "2040.1.1.1"			--base is 1949.1.1.1
 
-NDefines.NTechnology.BASE_TECH_COST = 100		--Defines what 1.0 units of research cost is equal to, base game is 110
+NDefines.NTechnology.BASE_TECH_COST = 100		--Defines what 1.0 units of research cost is equal to in days to research, base game is 110
 
 NDefines_Graphics.NGraphics.COUNTRY_COLOR_SATURATION_MODIFIER = 1.0		--Base is 0.6, defines the saturation of the country's RGB value on the map
 NDefines_Graphics.NGraphics.COUNTRY_COLOR_BRIGHTNESS_MODIFIER = 0.8		--Base is 0.8, defines the brightness of the country's RGB value on the map
