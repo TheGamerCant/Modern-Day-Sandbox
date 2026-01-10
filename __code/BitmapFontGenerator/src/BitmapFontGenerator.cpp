4f535aee763c9caf1c978ac943716803ba6bb36a
TheGamerCant <123405360+TheGamerCant@users.noreply.github.com>
2026-01-10 19:04:13 +0000

    Test Push

--- __code/BitmapFontGenerator/src/BitmapFontGenerator.cpp
+++ __code/BitmapFontGenerator/src/BitmapFontGenerator.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cmath>
 #include <fstream>
+#include "json.hpp"
+using json = nlohmann::json;
 
 #define STB_RECT_PACK_IMPLEMENTATION
 #include "stb_rect_pack.h"
@@ -30,22 +32,6 @@ static void AddCodepointRange(Font* font, const Char* fontPath, const SignedInte
     *font = LoadFontEx(fontPath, fontSize, updatedCodepoints, updatedCodepointCount);
     RL_FREE(updatedCodepoints);
 }
-
-struct FontToLoad {
-    String fontPath;
-    String fontName;
-    String fontFileName;
-    SignedInteger32 fontSize;
-    UnsignedInteger8 r, g, b, a;
-    SignedInteger32 outline, additionalAdvanceX;
-    Vector<UnsignedInteger32> charsToLoad;
-
-	FontToLoad() : fontPath(""), fontName(""), fontFileName(""), fontSize(0), r(255), g(255), b(255), a(255), outline(0), additionalAdvanceX(0), charsToLoad() {}
-	FontToLoad(const String& fontPath, const String& fontName, const String& fontFileName, const String& fontSize, const String& r, const String& g, const String& b, const String& a,
-        const String& outline, const String& additionalAdvanceX, const String& charsToLoad) :
-        fontPath(fontPath), fontName(fontName), fontFileName(fontFileName), fontSize(std::stoll(fontSize)), r(std::stoi(r)), g(std::stoi(g)), b(std::stoi(b)), a(std::stoi(a)),
-        outline(std::stoi(outline)), additionalAdvanceX(std::stoi(additionalAdvanceX)), charsToLoad(ParseStringAsUnsignedInteger32Array(charsToLoad)) {}
-};
 
 
 static void ImageDropShadow(Image& src, int blurRadius, Color shadowColor) {
@@ -131,10 +117,63 @@ static void SaveToDDS(const String& filename, const UnsignedInteger8* data, Unsi
     file.write(reinterpret_cast<const Char*>(data), width * height * 4);
 }
 
+struct SubFont {
+    String fontFileName;
+    SignedInteger32 fontSize;
+    UnsignedInteger8 r, g, b, a;
+    SignedInteger32 outline, additionalAdvanceX;
+    Vector<UnsignedInteger32> charsToLoad;
+
+    SubFont() : fontFileName(""), fontSize(0), r(255), g(255), b(255), a(255), outline(0), additionalAdvanceX(0), charsToLoad() {}
+    SubFont(const String& fontFileName, const SignedInteger32 fontSize, const UnsignedInteger8 r, const UnsignedInteger8 g, 
+        const UnsignedInteger8 b, const UnsignedInteger8 a, const SignedInteger32 outline, const SignedInteger32 additionalAdvanceX,
+        const Vector<UnsignedInteger32>& charsToLoad) :
+        fontFileName(fontFileName), fontSize(fontSize), r(r), g(g), b(b), a(a), outline(outline), additionalAdvanceX(additionalAdvanceX), charsToLoad(charsToLoad) {}
+};
+
+struct FontToLoad {
+    String fontPath;
+    String fontName;
+    HashMap<UnsignedInteger32, UnsignedInteger32> similarGlyphs;
+    Vector<SubFont> subFonts;
+
+    FontToLoad() : fontPath(""), fontName(""), similarGlyphs() {}
+    FontToLoad(const json& fontsData) : fontPath(fontsData.at("font_path").get<String>()), fontName(fontsData.at("font_name").get<String>()), similarGlyphs(), subFonts(){
+        if (fontsData.contains("similar_glyphs")) {
+            for (auto it = fontsData["similar_glyphs"].begin(); it != fontsData["similar_glyphs"].end(); ++it) {
+                similarGlyphs.emplace(
+                    std::stoi(it.key()), 
+                    it.value().get<SignedInteger32>()
+                );
+            }
+        }
+
+        if (!fontsData.contains("sub_fonts") || !fontsData["sub_fonts"].is_array()) { return; }
+
+        for (const auto& subFontEntry : fontsData["sub_fonts"]) {
+            subFonts.emplace_back(
+                subFontEntry.at("bitmap_font_name").get<String>(),
+                subFontEntry.at("size").get<SignedInteger32>(),
+                subFontEntry.at("red").get<UnsignedInteger8>(),
+                subFontEntry.at("green").get<UnsignedInteger8>(),
+                subFontEntry.at("blue").get<UnsignedInteger8>(),
+                subFontEntry.at("alpha").get<UnsignedInteger8>(),
+                subFontEntry.at("outline").get<SignedInteger32>(),
+                subFontEntry.at("additional_advance_x").get<SignedInteger32>(),
+                subFontEntry.at("chars").get<Vector<UnsignedInteger32>>()
+            );
+        }
+    }
+};
+
 int main(void) {
-    Vector<String>fontsData = ParseStringAsStringArray(LoadFileToString("in/fonts.txt"));
-	const SizeT expectedDataCount = 11;
-	const SizeT fontsCount = fontsData.size() / expectedDataCount;
+    std::ifstream file("in/fonts.json");
+    json fontsData = json::parse(file);
+
+    Vector<FontToLoad> fontsToLoad;
+    for (const auto& font : fontsData.at("fonts")) {
+        fontsToLoad.emplace_back(font);
+    }
 
     SetTraceLogLevel(LOG_NONE);
     SetConfigFlags(FLAG_WINDOW_HIDDEN);
@@ -142,141 +181,144 @@ int main(void) {
     SetTargetFPS(60);
 
     if (!std::filesystem::exists("out") || !std::filesystem::is_directory("out")) { std::filesystem::create_directory("out"); }
-    
 
-    for (SizeT i = 0; i < fontsCount * expectedDataCount; i += expectedDataCount) {
-        const FontToLoad fontToLoad(
-            fontsData[i + 0],
-            fontsData[i + 1],
-            fontsData[i + 2],
-            fontsData[i + 3],
-            fontsData[i + 4],
-            fontsData[i + 5],
-            fontsData[i + 6],
-            fontsData[i + 7],
-            fontsData[i + 8],
-            fontsData[i + 9],
-            fontsData[i + 10]
-        );
-        if (fontToLoad.charsToLoad.size() % 2 != 0) { 
-            std::cout << "Font " << fontToLoad.fontFileName << " must have an even number of chars to load.\n";
-            continue; 
-        }
+    for (const auto& font : fontsToLoad) {
+        for (const auto& subFont : font.subFonts) {
+            //Load font into raylib
+            Font raylibFont = LoadFontEx(font.fontPath.c_str(), subFont.fontSize, nullptr, 0);
+            for (SizeT charToLoadIndex = 0; charToLoadIndex < subFont.charsToLoad.size(); charToLoadIndex += 2) {
+                AddCodepointRange(
+                    &raylibFont,
+                    font.fontPath.c_str(),
+                    subFont.fontSize,
+                    subFont.charsToLoad[charToLoadIndex + 0],
+                    subFont.charsToLoad[charToLoadIndex + 1]
+                );
+            }
 
-        //Load font into raylib
-        Font raylibFont = LoadFontEx(fontToLoad.fontPath.c_str(), fontToLoad.fontSize, nullptr, 0);
-        for (SizeT charToLoadIndex = 0; charToLoadIndex < fontToLoad.charsToLoad.size(); charToLoadIndex += 2) {
-	    	AddCodepointRange(
-                &raylibFont, 
-                fontToLoad.fontPath.c_str(), 
-                fontToLoad.fontSize, 
-                fontToLoad.charsToLoad[charToLoadIndex + 0], 
-                fontToLoad.charsToLoad[charToLoadIndex + 1]
-            );
-        }
+            //Create map of letters
+			HashMap<UnsignedInteger32, UnsignedInteger32> letterIndexMap;
+            for (SizeT glyphIndex = 0; glyphIndex < raylibFont.glyphCount; glyphIndex++) {
+                const GlyphInfo& glyphInfo = raylibFont.glyphs[glyphIndex];
+                letterIndexMap.emplace(glyphInfo.value, static_cast<UnsignedInteger32>(glyphIndex));
+			}
 
-		//Create rectangles to pack
-        Vector<stbrp_rect> rects(raylibFont.glyphCount);
-        SizeT totalFontArea = 0;
-        const UnsignedInteger32 outline2 = fontToLoad.outline * 2;
+            //Create rectangles to pack
+            Vector<stbrp_rect> rects(raylibFont.glyphCount);
+            SizeT totalFontArea = 0;
+            const UnsignedInteger32 outline2 = subFont.outline * 2;
 
-        for (SizeT rectangleIndex = 0; rectangleIndex < raylibFont.glyphCount; rectangleIndex++) {
-			const GlyphInfo& glyphInfo = raylibFont.glyphs[rectangleIndex];
+            for (SizeT rectangleIndex = 0; rectangleIndex < raylibFont.glyphCount; rectangleIndex++) {
+                const GlyphInfo& glyphInfo = raylibFont.glyphs[rectangleIndex];
 
-            totalFontArea += static_cast<SizeT>(glyphInfo.image.width + outline2) * static_cast<SizeT>(glyphInfo.image.width + outline2);
-            rects[rectangleIndex] = {
-                .id = static_cast<int>(glyphInfo.value),
-                .w = static_cast<stbrp_coord>(glyphInfo.image.width + outline2),
-                .h = static_cast<stbrp_coord>(glyphInfo.image.height + outline2),
-                .x = 0,
-                .y = 0,
-                .was_packed = 0
+                totalFontArea += static_cast<SizeT>(glyphInfo.image.width + outline2) * static_cast<SizeT>(glyphInfo.image.width + outline2);
+                rects[rectangleIndex] = {
+                    .id = static_cast<int>(glyphInfo.value),
+                    .w = static_cast<stbrp_coord>(glyphInfo.image.width + outline2),
+                    .h = static_cast<stbrp_coord>(glyphInfo.image.height + outline2),
+                    .x = 0,
+                    .y = 0,
+                    .was_packed = 0
+                };
+            }
+
+            //Get the width and height of the total font image
+            const SignedInteger32 dimension = static_cast<SignedInteger32>(std::sqrt(totalFontArea) * 1.25f);
+            UnsignedInteger8* imgDataPtr = new UnsignedInteger8[static_cast<SizeT>(dimension) * static_cast<SizeT>(dimension) * 2]();
+
+            //Pack rectanges
+            Vector<stbrp_node> nodes(dimension);
+            stbrp_context context;
+            stbrp_init_target(&context, dimension, dimension, nodes.data(), dimension);
+            stbrp_pack_rects(&context, rects.data(), rects.size());
+
+
+            //Write image information to imgDataPtr
+            SizeT imgDataPtrIndex = 0, letterDataIndex = 0, rectIndex = 0;
+            for (const auto& rect : rects) {
+                const GlyphInfo& currentGlyph = raylibFont.glyphs[rectIndex];
+                imgDataPtrIndex = (static_cast<SizeT>(rect.y + subFont.outline) * static_cast<SizeT>(dimension) + rect.x + subFont.outline) * 2;
+                letterDataIndex = 0;
+
+                for (SizeT row = 0; row < currentGlyph.image.height; row++) {
+                    std::memcpy(&imgDataPtr[imgDataPtrIndex], static_cast<unsigned char*>(currentGlyph.image.data) + letterDataIndex, static_cast<SizeT>(currentGlyph.image.width) * 2);
+
+                    letterDataIndex += static_cast<SizeT>(currentGlyph.image.width) * 2;
+                    imgDataPtrIndex += static_cast<SizeT>(dimension) * 2;
+                }
+                rectIndex++;
+            }
+
+            Image outImg{
+                .data = imgDataPtr,
+                .width = dimension,
+                .height = dimension,
+                .mipmaps = 1,
+                .format = PIXELFORMAT_UNCOMPRESSED_GRAY_ALPHA
             };
-        }
 
-		//Get the width and height of the total font image
-        const SignedInteger32 dimension = static_cast<SignedInteger32>(std::sqrt(totalFontArea) * 1.25f);
-        UnsignedInteger8* imgDataPtr = new UnsignedInteger8[static_cast<SizeT>(dimension) * static_cast<SizeT>(dimension) * 2]();
+            ImageAlphaCrop(&outImg, 0.0f);
+            ImageFormat(&outImg, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
 
-        //Pack rectanges
-        Vector<stbrp_node> nodes(dimension);
-        stbrp_context context;
-        stbrp_init_target(&context, dimension, dimension, nodes.data(), dimension);
-        stbrp_pack_rects(&context, rects.data(), rects.size());
+            if (subFont.outline != 0) {
+                ImageResizeCanvas(
+                    &outImg,
+                    outImg.width + outline2,
+                    outImg.height + outline2,
+                    subFont.outline, subFont.outline,
+                    Color(0, 0, 0, 0)
+                );
 
-
-		//Write image information to imgDataPtr
-        SizeT imgDataPtrIndex = 0, letterDataIndex = 0, rectIndex = 0;
-        for (const auto& rect: rects) {
-            const GlyphInfo& currentGlyph = raylibFont.glyphs[rectIndex];
-            imgDataPtrIndex = (static_cast<SizeT>(rect.y + fontToLoad.outline) * static_cast<SizeT>(dimension) + rect.x + fontToLoad.outline) * 2;
-            letterDataIndex = 0;
-
-            for (SizeT row = 0; row < currentGlyph.image.height; row++) {
-                std::memcpy(&imgDataPtr[imgDataPtrIndex], static_cast<unsigned char*>(currentGlyph.image.data) + letterDataIndex, static_cast<SizeT>(currentGlyph.image.width) * 2);
-
-                letterDataIndex += static_cast<SizeT>(currentGlyph.image.width) * 2;
-                imgDataPtrIndex += static_cast<SizeT>(dimension) * 2;
+                for (SizeT i = 0; i < subFont.outline; i++) {
+                    ImageDropShadow(outImg, 1, Color(0, 0, 0, 255));
+                }
             }
-            rectIndex++;
-        }
 
-        Image outImg{
-            .data = imgDataPtr,
-            .width = dimension,
-            .height = dimension,
-            .mipmaps = 1,
-            .format = PIXELFORMAT_UNCOMPRESSED_GRAY_ALPHA
-        };
+            ImageColorTint(&outImg, Color(subFont.r, subFont.g, subFont.b, subFont.a));
 
-        ImageAlphaCrop(&outImg, 0.0f);
-        ImageFormat(&outImg, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
-
-        if (fontToLoad.outline != 0) {
-            ImageResizeCanvas(
-                &outImg,
-                outImg.width + outline2,
-                outImg.height + outline2,
-                fontToLoad.outline, fontToLoad.outline,
-                Color(0, 0, 0, 0)
+            SaveToDDS(
+                "out\\" + subFont.fontFileName + ".dds",
+                static_cast<const UnsignedInteger8*>(outImg.data),
+                outImg.width,
+                outImg.height
             );
 
-            for (SizeT i = 0; i < fontToLoad.outline; i++) {
-                ImageDropShadow(outImg, 1, Color(0, 0, 0, 255));
+
+            UnloadImage(outImg);
+
+            std::ofstream fntFile("out\\" + subFont.fontFileName + ".fnt", std::ios::binary);
+            String fntFileString = "info face=\"" + font.fontName + "\" size=-" + std::to_string(subFont.fontSize) + " bold=0 italic=0 charset=\"\" unicode=1 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=0,0 outline="
+                + std::to_string(subFont.outline) + "\ncommon lineheight=" + std::to_string(subFont.fontSize) + " base=" + std::to_string(static_cast<SignedInteger32>(std::round(0.79f * subFont.fontSize))) + " scaleW=" +
+                std::to_string(outImg.width) + " scaleH=" + std::to_string(outImg.height) + " pages=1 packed=0 alphaChnl=0 redChnl=0 greenChnl=0 blueChnl=0\npage id=0 file=\"" +
+                subFont.fontFileName + ".dds\"\nchars count=" + std::to_string(rects.size());
+
+
+            for (SizeT glyphIndex = 0; glyphIndex < raylibFont.glyphCount; glyphIndex++) {
+                const GlyphInfo& letter = raylibFont.glyphs[glyphIndex];
+                const stbrp_rect& letterData = rects[glyphIndex];
+
+				UnsignedInteger32 xOffset = letter.offsetX + subFont.outline;
+				UnsignedInteger32 xAdvance = letter.advanceX + subFont.outline + subFont.additionalAdvanceX;
+
+                /*
+                if (font.similarGlyphs.contains(letter.value)) {
+                    const UnsignedInteger32 similarGlyphValue = font.similarGlyphs.at(letter.value);
+                    const UnsignedInteger32 similarGlyphIndex = letterIndexMap.at(similarGlyphValue);
+                    const GlyphInfo& similarGlyph = raylibFont.glyphs[similarGlyphIndex];
+                    xOffset = similarGlyph.offsetX + subFont.outline;
+                    xAdvance = similarGlyph.advanceX + subFont.outline + subFont.additionalAdvanceX;
+				}
+                */
+
+                fntFileString += "\nchar id=" + std::to_string(letter.value) + " x=" + std::to_string(letterData.x) + " y=" + std::to_string(letterData.y) +
+                    " width=" + std::to_string(letterData.w) + " height=" + std::to_string(letterData.h) + " xoffset=" + std::to_string(xOffset) +
+                    " yoffset=" + std::to_string(letter.offsetY + subFont.outline) + " xadvance=" + std::to_string(xAdvance) + " page=0 chnl=15";
             }
+
+            fntFile << fntFileString;
+            UnloadFont(raylibFont);
         }
-
-        ImageColorTint(&outImg, Color(fontToLoad.r, fontToLoad.g, fontToLoad.b, fontToLoad.a));
-
-        SaveToDDS(
-            "out\\" + fontToLoad.fontFileName + ".dds",
-            static_cast<const UnsignedInteger8*>(outImg.data),
-            outImg.width,
-            outImg.height
-        );
-
-        
-		UnloadImage(outImg);
-
-        std::ofstream fntFile("out\\" + fontToLoad.fontFileName + ".fnt", std::ios::binary);
-        String fntFileString = "info face=\"" + fontToLoad.fontName + "\" size=-" + std::to_string(fontToLoad.fontSize) + " bold=0 italic=0 charset=\"\" unicode=1 stretchH=100 smooth=1 aa=1 padding=0,0,0,0 spacing=0,0 outline="
-            + std::to_string(fontToLoad.outline) + "\ncommon lineheight=" + std::to_string(fontToLoad.fontSize) + " base=" + std::to_string(static_cast<SignedInteger32>(std::round(0.79f * fontToLoad.fontSize))) + " scaleW=" +
-            std::to_string(outImg.width) + " scaleH=" + std::to_string(outImg.height) + " pages=1 packed=0 alphaChnl=0 redChnl=0 greenChnl=0 blueChnl=0\npage id=0 file=\"" +
-            fontToLoad.fontFileName + ".dds\"\nchars count=" + std::to_string(rects.size());
-
-        for (SizeT glyphIndex = 0; glyphIndex < raylibFont.glyphCount; glyphIndex++) {
-			const GlyphInfo& letter = raylibFont.glyphs[glyphIndex];
-			const stbrp_rect& letterData = rects[glyphIndex];
-
-            fntFileString += "\nchar id=" + std::to_string(letter.value) + " x=" + std::to_string(letterData.x) + " y=" + std::to_string(letterData.y) +
-                " width=" + std::to_string(letterData.w) + " height=" + std::to_string(letterData.h) + " xoffset=" + std::to_string(int(letter.offsetX + fontToLoad.outline)) +
-                " yoffset=" + std::to_string(int(letter.offsetY + fontToLoad.outline)) + " xadvance=" + std::to_string(int(letter.advanceX + fontToLoad.outline + fontToLoad.additionalAdvanceX)) + " page=0 chnl=15";
-        }
-
-        fntFile << fntFileString;
-        UnloadFont(raylibFont);
-	}
-
+    }
 
     return 0;
 }
