ad78b7b57e291c6905938a021ec10dba3bb807d4
charles-siteassist <charles@siteassist.co>
2026-03-05 14:02:07 +0000

    Fixes and more stuff

--- __code/culture_generator/culture_main.py
+++ __code/culture_generator/culture_main.py
@@ -26,7 +26,86 @@ class Culture:
             f"Green: {self.green}\nBlue: {self.blue}\nSuper Culture: {self.super_culture}\n"
         )
 
-def WriteOnActionsFile
+def RgbToInteger(red: int, green: int, blue: int) -> int:
+    return (red * 256 * 256) + (green * 256) + blue
+
+def WriteOnActionsFile(cultures_list: list[Culture], on_actions_file: Path) -> None:
+    with open(str(on_actions_file), "w", encoding="utf-8") as f:
+        f.write(
+            "on_actions = {\n\ton_startup = {\n\t\teffect = {\n\n\t\t\t"
+            f"resize_array = {{ array = global.culture_colour_array size = {len(cultures_list)} value = 0 }}\n\t\t\t"
+			f"resize_array = {{ array = global.culture_names_array  size = {len(cultures_list)} value = 0 }}\n\t\t\t"
+			f"resize_array = {{ array = global.culture_desc_array   size = {len(cultures_list)} value = 0 }}\n\t\t\t"
+        )
+
+        for i, culture in enumerate(cultures_list):
+            f.write(
+                f"set_variable = {{  global.culture_names_array^{i} = token:TDA_culture_{culture.token}_token_idea }}\n\t\t\t"
+                f"set_variable = {{   global.culture_desc_array^{i} = token:TDA_culture_{culture.token}_desc_token_idea }}\n\t\t\t"
+                f"#{culture.red}, {culture.green}, {culture.blue}\n\t\t\t"
+                f"set_variable = {{ global.culture_colour_array^{i} = {RgbToInteger(culture.red, culture.green, culture.blue)} }}\n\n\t\t\t"
+            )
+
+        f.write(
+            "#Do this after setting up the colours\n\t\t\tZZZ = { update_every_state_culture = yes }"
+            "\n\t\t}\n\t}\n}"
+        )
+
+def WriteIdeasFile(cultures_list: list[Culture], ideas_file: Path) -> None:
+    with open(str(ideas_file), "w", encoding="utf-8") as f:
+        f.write(
+            "ideas = {\n\tcountry = {"
+        )
+
+        for culture in cultures_list:
+            f.write(
+                f"\n\t\tTDA_culture_{culture.token}_token_idea = {{}}"
+                f"\n\t\tTDA_culture_{culture.token}_desc_token_idea = {{}}\n"
+            )
+
+        f.write(
+            "\t}\n}"
+        )
+
+def WriteLocalisationFile(cultures_list: list[Culture], localisation_file: Path) -> None:
+    with open(str(localisation_file), "w", encoding="utf-8") as f:
+        f.write(
+"""l_english:
+ MAPMODE_TDA_CULTURE_MAP_MODE:0 "§YCulture§! map mode"
+ MAPMODE_TDA_CULTURE_MAP_MODE_NAME:0 "Culture"
+ MAPMODE_TDA_CULTURE_MAP_MODE_DESCRIPTION:0 ""
+
+ TDA_culture_map_mode_tooltip:0 "[GetCultureMapModeTT]"
+ TDA_culture_map_mode_tooltip_delayed:0 "[GetCultureMapModeTTDelayed]"
+ 
+ TDA_culture_state_is_homogenous:0 "The culture of §Y[FROM.GetName]§! is [?global.culture_names_array^dem1.GetTokenLocalizedKey]."
+ TDA_culture_state_is_nearly_homogenous:0 "The culture of §Y[FROM.GetName]§! is almost entirely [?global.culture_names_array^dem1.GetTokenLocalizedKey]."
+ TDA_culture_state_has_large_majority_no_minority:0 "The culture of §Y[FROM.GetName]§! is overwhelmingly [?global.culture_names_array^dem1.GetTokenLocalizedKey]."
+ TDA_culture_state_has_large_majority_one_minority:0 "The culture of §Y[FROM.GetName]§! is overwhelmingly [?global.culture_names_array^dem1.GetTokenLocalizedKey], with a large [?global.culture_names_array^dem2.GetTokenLocalizedKey] minority."
+ TDA_culture_state_has_large_majority_two_minorities:0 "The culture of §Y[FROM.GetName]§! is overwhelmingly [?global.culture_names_array^dem1.GetTokenLocalizedKey], with large [?global.culture_names_array^dem2.GetTokenLocalizedKey] and [?global.culture_names_array^dem3.GetTokenLocalizedKey] minorities."
+ TDA_culture_state_has_simple_majority_no_minority:0 "The culture of §Y[FROM.GetName]§! is majority [?global.culture_names_array^dem1.GetTokenLocalizedKey]."
+ TDA_culture_state_has_simple_majority_one_minority:0 "The culture of §Y[FROM.GetName]§! is majority [?global.culture_names_array^dem1.GetTokenLocalizedKey], with a large [?global.culture_names_array^dem2.GetTokenLocalizedKey] minority."
+ TDA_culture_state_has_simple_majority_two_minorities:0"The culture of §Y[FROM.GetName]§! is majority [?global.culture_names_array^dem1.GetTokenLocalizedKey], with large [?global.culture_names_array^dem2.GetTokenLocalizedKey] and [?global.culture_names_array^dem3.GetTokenLocalizedKey] minorities."
+ TDA_culture_state_has_simple_majority_three_minorities:0"The culture of §Y[FROM.GetName]§! is majority [?global.culture_names_array^dem1.GetTokenLocalizedKey], with large [?global.culture_names_array^dem2.GetTokenLocalizedKey], [?global.culture_names_array^dem3.GetTokenLocalizedKey] and [?global.culture_names_array^dem4.GetTokenLocalizedKey] minorities."
+ TDA_culture_state_has_plurality_no_minority:0 "The culture of §Y[FROM.GetName]§! is plurality [?global.culture_names_array^dem1.GetTokenLocalizedKey]."
+ TDA_culture_state_has_plurality_one_minority:0 "The culture of §Y[FROM.GetName]§! is plurality [?global.culture_names_array^dem1.GetTokenLocalizedKey], with a large [?global.culture_names_array^dem2.GetTokenLocalizedKey] minority."
+ TDA_culture_state_has_plurality_two_minorities:0"The culture of §Y[FROM.GetName]§! is plurality [?global.culture_names_array^dem1.GetTokenLocalizedKey], with large [?global.culture_names_array^dem2.GetTokenLocalizedKey] and [?global.culture_names_array^dem3.GetTokenLocalizedKey] minorities."
+ TDA_culture_state_has_plurality_three_minorities:0"The culture of §Y[FROM.GetName]§! is plurality [?global.culture_names_array^dem1.GetTokenLocalizedKey], with large [?global.culture_names_array^dem2.GetTokenLocalizedKey], [?global.culture_names_array^dem3.GetTokenLocalizedKey] and [?global.culture_names_array^dem4.GetTokenLocalizedKey] minorities."
+ TDA_culture_state_mostly_other:0 "The culture of §Y[FROM.GetName]§! has no clear cultural majority or plurality."
+ 
+ TDA_culture_map_mode_1_row:0 "[?global.culture_names_array^loc_row_1.GetTokenLocalizedKey] - [?value_row_1|2%%]"
+ TDA_culture_map_mode_2_row:0 "[?global.culture_names_array^loc_row_1.GetTokenLocalizedKey] - [?value_row_1|2%%]\\n[?global.culture_names_array^loc_row_2.GetTokenLocalizedKey] - [?value_row_2|2%%]"
+ TDA_culture_map_mode_3_row:0 "[?global.culture_names_array^loc_row_1.GetTokenLocalizedKey] - [?value_row_1|2%%]\\n[?global.culture_names_array^loc_row_2.GetTokenLocalizedKey] - [?value_row_2|2%%]\\n[?global.culture_names_array^loc_row_3.GetTokenLocalizedKey] - [?value_row_3|2%%]"
+ TDA_culture_map_mode_4_row:0 "[?global.culture_names_array^loc_row_1.GetTokenLocalizedKey] - [?value_row_1|2%%]\\n[?global.culture_names_array^loc_row_2.GetTokenLocalizedKey] - [?value_row_2|2%%]\\n[?global.culture_names_array^loc_row_3.GetTokenLocalizedKey] - [?value_row_3|2%%]\\n[?global.culture_names_array^loc_row_4.GetTokenLocalizedKey] - [?value_row_4|2%%]"
+ TDA_culture_map_mode_5_row:0 "[?global.culture_names_array^loc_row_1.GetTokenLocalizedKey] - [?value_row_1|2%%]\\n[?global.culture_names_array^loc_row_2.GetTokenLocalizedKey] - [?value_row_2|2%%]\\n[?global.culture_names_array^loc_row_3.GetTokenLocalizedKey] - [?value_row_3|2%%]\\n[?global.culture_names_array^loc_row_4.GetTokenLocalizedKey] - [?value_row_4|2%%]\\n[?global.culture_names_array^loc_row_5.GetTokenLocalizedKey] - [?value_row_5|2%%]"
+ TDA_culture_map_mode_6_row:0 "[?global.culture_names_array^loc_row_1.GetTokenLocalizedKey] - [?value_row_1|2%%]\\n[?global.culture_names_array^loc_row_2.GetTokenLocalizedKey] - [?value_row_2|2%%]\\n[?global.culture_names_array^loc_row_3.GetTokenLocalizedKey] - [?value_row_3|2%%]\\n[?global.culture_names_array^loc_row_4.GetTokenLocalizedKey] - [?value_row_4|2%%]\\n[?global.culture_names_array^loc_row_5.GetTokenLocalizedKey] - [?value_row_5|2%%]\\n[?global.culture_names_array^loc_row_6.GetTokenLocalizedKey] - [?value_row_6|2%%]"
+ TDA_culture_map_mode_7_row:0 "[?global.culture_names_array^loc_row_1.GetTokenLocalizedKey] - [?value_row_1|2%%]\\n[?global.culture_names_array^loc_row_2.GetTokenLocalizedKey] - [?value_row_2|2%%]\\n[?global.culture_names_array^loc_row_3.GetTokenLocalizedKey] - [?value_row_3|2%%]\\n[?global.culture_names_array^loc_row_4.GetTokenLocalizedKey] - [?value_row_4|2%%]\\n[?global.culture_names_array^loc_row_5.GetTokenLocalizedKey] - [?value_row_5|2%%]\\n[?global.culture_names_array^loc_row_6.GetTokenLocalizedKey] - [?value_row_6|2%%]\\n[?global.culture_names_array^loc_row_7.GetTokenLocalizedKey] - [?value_row_7|2%%]\""""
+)
+        for culture in cultures_list:
+            f.write(
+                f"\n\n TDA_culture_{culture.token}_token_idea:0 \"{culture.name}\""
+                f"\n TDA_culture_{culture.token}_desc_token_idea:0 \"{culture.desc}\""
+            )
 
 def LoadCulturesFromJson() -> list[Culture]:
     culture_list: list[Culture] = []
@@ -58,8 +137,9 @@ def main():
 
     cultures_list: list[Culture] = LoadCulturesFromJson()
 
-    for culture in cultures_list:
-        print(culture)
+    WriteOnActionsFile(cultures_list, on_actions_file)
+    WriteIdeasFile(cultures_list, idea_tokens_file)
+    WriteLocalisationFile(cultures_list, localisation_file)
 
 
 if __name__ == "__main__":
